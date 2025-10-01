c7cff3f7c87f58584c1666d2e37b00319f601078
Marie <marie.roche32@orange.fr>
2025-10-01 22:42:53 +0200

    [ADD] some css adjustments

--- components/Home.js
+++ components/Home.js
@@ -7,13 +7,16 @@ function Home() {
     <div className={styles.container}>
       <div className={styles.bgImage}>
         <h3 className={styles.title}>Weather App</h3>
-        <div className={styles.middle}>
-          <input id="cityNameInput" type="text" placeholder="Search City" />
-          <button id="addCity"><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
+        <div className={styles.centerWrapper}>
+          <div className={styles.middle}>
+            <input className={styles.input} id="cityNameInput" type="text" placeholder="Search City" />
+            <button className={styles.btn} id="addCity"><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
+          </div>
         </div>
       </div>
     </div>
   );
 }
+
 
 export default Home;
