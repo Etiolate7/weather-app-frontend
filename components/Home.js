b04c40016f47d6a1edbae3256d9971556ed1b515
Marie <marie.roche32@orange.fr>
2025-10-01 19:54:38 +0200

    [ADD] basic front

--- components/Home.js
+++ components/Home.js
@@ -1,13 +1,17 @@
 import styles from '../styles/Home.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 function Home() {
   return (
-    <div>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
+    <div className={styles.container}>
+      <div className={styles.bgImage}>
+        <h3 className={styles.title}>Weather App</h3>
+        <div className={styles.middle}>
+          <input id="cityNameInput" type="text" placeholder="Search City" />
+          <button id="addCity"><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
+        </div>
+      </div>
     </div>
   );
 }
