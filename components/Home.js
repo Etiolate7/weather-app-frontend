879b42eb60c0e2a50877a4f99cfa56c6bf033db8
Marie <marie.roche32@orange.fr>
2025-10-03 00:03:21 +0200

    [ADD] displaying api informations

--- components/Home.js
+++ components/Home.js
@@ -1,22 +1,73 @@
-import styles from '../styles/Home.module.css';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import styles from '../styles/Home.module.css';
+import { useState } from 'react';
 
-function Home() {
+const Home = () => {
+
+  const [cityNameInput, setCityNameInput] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleChange = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/weather", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ cityName: cityNameInput }),
+      });
+
+      const data = await response.json();
+
+      if (data.result) {
+        setWeatherData(data.weather);
+        setError(null);
+      } else {
+        setError(data.error);
+        setWeatherData(null);
+      }
+    } catch (err) {
+      setError("Error");
+      setWeatherData(null);
+    }
+  };
+
+  console.log(cityNameInput);
+
   return (
     <div className={styles.container}>
       <div className={styles.bgImage}>
         <h3 className={styles.title}>Weather App</h3>
         <div className={styles.centerWrapper}>
           <div className={styles.middle}>
-            <input className={styles.input} id="cityNameInput" type="text" placeholder="Search City" />
-            <button className={styles.btn} id="addCity"><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
+            <input
+              className={styles.input}
+              id="cityNameInput"
+              type="text"
+              placeholder="Search City"
+              value={cityNameInput} onChange={e => setCityNameInput(e.target.value)}
+            />
+            <button onClick={handleChange} className={styles.btn} id="addCity">
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </button>
+            {weatherData && (
+              <div className={styles.weatherInfo}>
+                <h4 className={styles.cityName}>{weatherData.cityName}</h4>
+                <p className={styles.description}>{weatherData.description}</p>
+                <p className={styles.tempMin}>Min: {weatherData.tempMin}°C</p>
+                <p className={styles.tempMax}>Max: {weatherData.tempMax}°C</p>
+              </div>
+            )}
+
+            {error && <p className={styles.error}>{error}</p>}
           </div>
         </div>
       </div>
     </div>
   );
-}
-
+};
 
 export default Home;
