24aebcde2edfdd18198120279b6e433a62a87511
Marie <marie.roche32@orange.fr>
2025-10-03 22:51:09 +0200

    [ADD&FIX] delete button, css

--- components/Home.js
+++ components/Home.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faMagnifyingGlass, faTrash } from '@fortawesome/free-solid-svg-icons';
 import styles from '../styles/Home.module.css';
-import { useState } from 'react';
 
 const Home = () => {
-
   const [cityNameInput, setCityNameInput] = useState("");
-  const [weatherData, setWeatherData] = useState(null);
+  const [weatherList, setWeatherList] = useState([]);
   const [error, setError] = useState(null);
 
-  const handleChange = async () => {
+  useEffect(() => {
+    fetch("http://localhost:3000/weather")
+      .then(res => res.json())
+      .then(data => {
+        setWeatherList(data.weather || []);
+      });
+  }, []);
+
+  const handleSearch = async () => {
     try {
       const response = await fetch("http://localhost:3000/weather", {
         method: "POST",
@@ -23,46 +29,85 @@ const Home = () => {
       const data = await response.json();
 
       if (data.result) {
-        setWeatherData(data.weather);
+        setWeatherList(prev => [...prev, data.weather]);
         setError(null);
+        setCityNameInput("");
       } else {
         setError(data.error);
-        setWeatherData(null);
       }
     } catch (err) {
-      setError("Error");
-      setWeatherData(null);
+      setError("Error fetching weather data.");
     }
   };
 
-  console.log(cityNameInput);
+  function capitalizeFirstLetter(str) {
+  if (!str) return "";
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+
+
+  const handleDelete = async (cityName) => {
+    try {
+      const response = await fetch(`http://localhost:3000/weather/${cityName}`, {
+        method: "DELETE",
+      });
+
+      const data = await response.json();
+
+      if (data.result) {
+        setWeatherList(data.weather);
+        setError(null);
+      } else {
+        setError(data.error);
+      }
+    } catch (err) {
+      setError("Error deleting city.");
+    }
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.bgImage}>
         <h3 className={styles.title}>Weather App</h3>
         <div className={styles.centerWrapper}>
-          <div className={styles.middle}>
+          <div
+            className={`${styles.middle} ${weatherList.length === 0 ? styles.middleCentered : ""
+              }`}
+          >
+            <div className={styles.search}>
             <input
               className={styles.input}
-              id="cityNameInput"
               type="text"
               placeholder="Search City"
-              value={cityNameInput} onChange={e => setCityNameInput(e.target.value)}
+              value={cityNameInput}
+              onChange={(e) => setCityNameInput(e.target.value)}
             />
-            <button onClick={handleChange} className={styles.btn} id="addCity">
+            <button onClick={handleSearch} className={styles.btn}>
               <FontAwesomeIcon icon={faMagnifyingGlass} />
             </button>
-            {weatherData && (
-              <div className={styles.weatherInfo}>
-                <h4 className={styles.cityName}>{weatherData.cityName}</h4>
-                <p className={styles.description}>{weatherData.description}</p>
-                <p className={styles.tempMin}>Min: {weatherData.tempMin}°C</p>
-                <p className={styles.tempMax}>Max: {weatherData.tempMax}°C</p>
-              </div>
-            )}
+            </div>
 
             {error && <p className={styles.error}>{error}</p>}
+
+            <div className={styles.weatherList}>
+              {weatherList.map((weather, index) => (
+                <div key={index} className={styles.weatherInfo}>
+                  <div className={styles.weatherHeader}>
+                    <h4 className={styles.cityName}>{weather.cityName}</h4>
+                    <button
+                      className={styles.deleteBtn}
+                      onClick={() => handleDelete(weather.cityName)}>
+                      <FontAwesomeIcon style={{
+                        filter: 'drop-shadow(1px 1px 2px rgba(0, 0, 0, 0.38))'
+                      }} icon={faTrash} />
+                    </button>
+                  </div>
+                  <p className={styles.description}>{capitalizeFirstLetter(weather.description)}</p>
+                  <p className={styles.tempMin}>Min: {weather.tempMin}°C</p>
+                  <p className={styles.tempMax}>Max: {weather.tempMax}°C</p>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
